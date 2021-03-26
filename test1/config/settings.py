a4a3ede855e88ad4b25b226221d3811f9dd1bb17
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-03-26 14:35:23 -0700

    add django-extensions

--- test1/config/settings.py
+++ test1/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # third party
     'rest_framework',
+    'django_extensions',
     # first party
     'blog.apps.BlogConfig',
     'organizer.apps.OrganizerConfig',
