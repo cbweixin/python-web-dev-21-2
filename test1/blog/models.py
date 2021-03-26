37718dfdb9055a5f9f097c2a4d9ba97b028644ef
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-03-26 14:57:10 -0700

    make new changes

--- test1/blog/models.py
+++ test1/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from test1.organizer.models import Tag
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=63)
+    slug = models.CharField(max_length=63)
+    text = models.TextField()
+    pub_date = models.DateField()
+    tags = models.ManyToManyField(Tag)
+
+
