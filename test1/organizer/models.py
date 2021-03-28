c552991cc5e4300596cc9355369563d7f3255daa
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-03-28 13:57:03 -0700

    add some new changes

--- test1/organizer/models.py
+++ test1/organizer/models.py
@@ -5,6 +5,9 @@ class Tag(models.Model):
     name = models.CharField(max_length=31, unique=True)
     slug = models.CharField(max_length=31, unique=True, help_text='A label for url config')
 
+    def __str__(self):
+        return self.name
+
 class Startup(models.Model):
     name = models.CharField(max_length=31,db_index=True)
     slug = models.CharField(max_length=31, unique=True,help_text='A label for url config')
@@ -13,6 +16,8 @@ class Startup(models.Model):
     contact = models.EmailField(max_length=255)
     website = models.URLField()
     tags = models.ManyToManyField(Tag)
+    def __str__(self):
+        return self.name
 
 class NewsLink(models.Model):
     title = models.CharField(max_length=31)
@@ -20,3 +25,5 @@ class NewsLink(models.Model):
     pub_date = models.DateField('date published')
     link = models.URLField(max_length=255)
     startup = models.ForeignKey(Startup, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.startup} : {self.title}"
